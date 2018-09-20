d6bbab6f1905a667288164b8744efda096cf9602
HanxunHuangLemonBear <curtishuang@foxmail.com>
2018-09-20 23:25:49 +1000

    Basic Server Function

--- src/scrabbleGameServer.java
+++ src/scrabbleGameServer.java
@@ -8,17 +8,36 @@ import java.util.*;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.InetAddress;
 import java.io.*;
 
 public class scrabbleGameServer {
     /*
-     * Connected Client Class
+     * Connected Player Client Class
      */
-    class connectedClient{
+    public static class connectedPlayerClient{
         public int userID;
         public String username;
-        public String userIP;
+        public InetAddress userIP;
+        public String userIPString;
+        public Socket socket;
+
     }
+
+    /*
+     * Game Room Class
+     */
+    public static class gameRoom{
+        private int minimalPlayersPerGame = 2;
+        private int maximumPlayersPerGame = 4;
+        public ArrayList<connectedPlayerClient> connectedPlayers = new ArrayList<>();
+        public scrabbleGame game;
+        public gameRoom() {
+            this.game = new scrabbleGame();
+        }
+    }
+
 
     /*
     * Initializing the Scrabble Game Server
@@ -29,12 +48,41 @@ public class scrabbleGameServer {
     private static int maximumPlayersPerGame = 4;
     private static orcaLogerHelper loggerHandler = new orcaLogerHelper();
     private static Logger logger = loggerHandler.getLogger();
-    private ArrayList<connectedClient> clientList = new ArrayList<>();
-    private scrabbleGame game;
+    private static ArrayList<connectedPlayerClient> clientList = new ArrayList<>();
+    private static ServerSocket serverSocket = null;
 
-    public scrabbleGameServer() {
-        game = new scrabbleGame();
+
+    /*
+     * Helper function
+     */
+    private static boolean checkNextArgumentStatus(String[] args, int i) {
+        /* Check if command line args at i have next argument */
+        if (i + 1 >= args.length || args[i + 1] == null) {
+            logger.severe("Bad argument, Check Manual!");
+            return false;
+        }
+        return true;
     }
+
+    private static void setServerBaseOnConfig(String filePath) {
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader(filePath));
+            JSONObject jsonObject = (JSONObject) obj;
+            if (jsonObject.has("server_port")) {
+                defaultPort = jsonObject.getInt("server_port");
+            }
+            if (jsonObject.has("minimal_players_per_game")) {
+                minimalPlayersPerGame = jsonObject.getInt("minimal_players_per_game");
+            }
+            if (jsonObject.has("maximum_players_per_game")) {
+                maximumPlayersPerGame = jsonObject.getInt("maximum_players_per_game");
+            }
+        } catch (Exception e) {
+            logger.severe(e.getClass().getSimpleName() + ": " + e.getMessage());
+        }
+    }
+
 
     /*
      * Main
@@ -58,45 +106,70 @@ public class scrabbleGameServer {
                 }
             }
         }
+
+        // Start The Server
+        gameServer(defaultPort);
+
     }
 
-
-    /*
-     * Helper function
-     */
-    private static boolean checkNextArgumentStatus(String[] args, int i) {
-        /* Check if command line args at i have next argument */
-        if (i + 1 >= args.length || args[i + 1] == null) {
-            logger.severe("Bad argument, Check Manual!");
-            return false;
-        }
-        return true;
-    }
-
-    public static void setServerBaseOnConfig(String filePath) {
-        try {
-            JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new FileReader(filePath));
-            JSONObject jsonObject = (JSONObject) obj;
-            if (jsonObject.has("server_port")) {
-                defaultPort = jsonObject.getInt("server_port");
-            }
-            if (jsonObject.has("minimal_players_per_game")) {
-                minimalPlayersPerGame = jsonObject.getInt("minimal_players_per_game");
-            }
-            if (jsonObject.has("maximum_players_per_game")) {
-                maximumPlayersPerGame = jsonObject.getInt("maximum_players_per_game");
-            }
-        } catch (Exception e) {
-            logger.severe(e.getClass().getSimpleName() + ": " + e.getMessage());
-        }
-    }
 
 
     /*
      * Socket Server
      */
-    public static void  gameServer(){
 
+    private static synchronized int getClientCounter() {
+        return clientCounter;
+    }
+
+    private static synchronized void incrementClientCounter(){
+        clientCounter = clientCounter + 1;
+    }
+
+    private static synchronized void decrementClientCounter(){
+        clientCounter = clientCounter - 1;
+    }
+
+    private static synchronized void resetClientCounter(int n){
+        clientCounter = n;
+    }
+
+
+    public static class clientHandlerThread implements Runnable {
+        connectedPlayerClient clientObject;
+
+        public clientHandlerThread(connectedPlayerClient clientObject) {
+            this.clientObject = clientObject;
+        }
+
+        public void run() {
+            clientHandler();
+        }
+
+        public void clientHandler() {
+            
+        }
+    }
+
+    private static void gameServer(int port){
+        try {
+            serverSocket = new ServerSocket(port);
+            logger.info("Server started on port: " + String.valueOf(port));
+            while (true) {
+                Socket socket = serverSocket.accept();
+                logger.info("Client ID: " + String.valueOf(getClientCounter()) + " accepted");
+                connectedPlayerClient client = new connectedPlayerClient();
+                client.socket = socket;
+                client.userID = getClientCounter();
+                client.userIP = socket.getInetAddress();
+                client.userIPString = client.userIP.getHostAddress();
+                clientList.add(client);
+                clientHandlerThread clientThread = new clientHandlerThread(client);
+                clientThread.run();
+                incrementClientCounter();
+            }
+        } catch (Exception e) {
+            logger.severe(e.getClass().getSimpleName() + ": " + e.getMessage());
+        }
     }
 }
